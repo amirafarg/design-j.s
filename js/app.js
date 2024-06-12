9189c341ac355a9f55923abc4866803357bf38b5
amirafarag <amiraazam301@gmail.com>
2024-06-12 06:09:22 +0300

    toggle

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
+// header
 const btnEnter = document.getElementById('toggel-enter');
-const menue = document.getElementById('column-div');
+const menue = document.getElementById('nav-list');
+const btnExit = document.getElementById('toggel-exit');
 
 btnEnter.addEventListener('click', toggleEnter);
 
 function toggleEnter () {
-    menue.classList.toggle('show');
+    menue.classList.add('show');
+    
+}
+
+
+// pragraph-sec-2
+const enterMinus = getElementById("minus");
+const paraDis = getElementById("p-t");
+
+enterMinus.addEventListener('click', disAppere);
+
+function disAppere () {
+    paraDis.classList.toggle('show');
 }
